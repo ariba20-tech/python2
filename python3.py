a72c51f63a33ae9714f96d3e079753d4e3c6cb43
ariba20-tech <aribahurrainhussain@gmail.com>
2025-04-25 09:32:57 +0530

    Update and rename python3 to python3.py

---
+++ python3.py
@@ -0,0 +1,14 @@
+def add(x, y):
+  return x + y
+
+def subtract(x, y):
+  return x - y
+
+num1 = 10
+num2 = 5
+
+sum_result = add(num1, num2)
+difference_result = subtract(num1, num2)
+
+print("Sum:", sum_result)
+print("Difference:", difference_result)
